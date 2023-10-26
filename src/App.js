540e06ac695437014a74fdb94ec7e014625752b4
Lovro Božić <106404810+Limi96limi@users.noreply.github.com>
2023-10-26 23:57:59 +0200

    fetch backend data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import { QueryParamsCalendarController } from "./QueryParamsCalendarController";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  axios
+    .get(
+      "https://api.github.com/repos/testing-library/react-testing-library/commits"
+    )
+    .then((response) => {
+      console.log(response.data);
+    });
   return (
     <BrowserRouter>
       <QueryParamsCalendarController />
